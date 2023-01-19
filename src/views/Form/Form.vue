49c1c1df0c456525eb7f5a62da341754b82b73d6
Zkeq <learnonly@learnonly.xyz>
2023-01-19 13:47:44 +0000

    Perf(Form.vue):optimized import.... code

--- src/views/Form/Form.vue
+++ src/views/Form/Form.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
     //引入KForm组件
 <<<<<<< HEAD
+<<<<<<< HEAD
     import KForm from "@/components/kform/KForm.vue"
 =======
     import KForm from "../../components/kform/KForm.vue"
 >>>>>>> 1d0b9f4 (Fix(Form.vue): Fix an introduction path error)
+=======
+    import KForm from "@/components/kform/KForm.vue"
+>>>>>>> 6d344de (Feat(Vite.config): add rule "@  is an alias to /src")
 </script>
 <template>
     <k-form></k-form>
