65540a84d2e603d1128e8651009edb28276b6dbc
Zkeq <learnonly@learnonly.xyz>
2023-01-19 13:30:19 +0000

    Fix(KForm.vue):have a path error already to be fixed

--- src/views/Form/Form.vue
+++ src/views/Form/Form.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
     //引入KForm组件
+<<<<<<< HEAD
     import KForm from "@/components/kform/KForm.vue"
+=======
+    import KForm from "../../components/kform/KForm.vue"
+>>>>>>> 1d0b9f4 (Fix(Form.vue): Fix an introduction path error)
 </script>
 <template>
     <k-form></k-form>
