5ea973067bf47e318a2138e1a46652d166b997c5
Huangpeijin <kim475@163.com>
2023-02-09 23:53:05 +0800

    Feat:The tree component is simply encapsulated

---
+++ src/views/Tree/doc/doc.md
@@ -0,0 +1,13 @@
+<script setup>
+    import demo1 from './demo1.vue' 
+    // import demo2 from './demo2.vue' 
+    // import demo3 from './demo3.vue'
+    // import demo4 from './demo4.vue'
+    // import demo5 from './demo5.vue'
+    // import demo6 from './demo6.vue'
+</script>
+## Tree树形控件
+
+### ● 基本使用
+<p> tree 组件基本使用示例。</p>
+<demo1/>
--- src/views/index.ts
+++ src/views/index.ts
@@ -3,8 +3,9 @@ import kTextarea from "./Textarea/index";
 import kPreview from "./Preview/index";
 import kSpace from "./Space/index";
 import kRadio from "./Radio/index";
+import kTree from "./Tree/index";
 
-const component =[kButton,kTextarea,kPreview,kSpace,kRadio];
+const component =[kButton,kTextarea,kPreview,kSpace,kRadio,kTree];
 const install =(app:any)=>{
     component.forEach((item)=>{
         app.use(item);
@@ -13,5 +14,5 @@ const install =(app:any)=>{
  const TUI = {
     install,
  }
- export {kButton,kTextarea,kPreview,kSpace,kRadio};
+ export {kButton,kTextarea,kPreview,kSpace,kRadio,kTree};
  export default TUI;
