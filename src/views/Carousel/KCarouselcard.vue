ee536abd5877fae9aa636bba38e96c73fb7348a1
EnJoy77 <echen7677@gmail.com>
2023-02-11 22:09:25 +0800

    repair carousel

--- src/views/Carousel/KCarouselcard.vue
+++ src/views/Carousel/KCarouselcard.vue
@@ -31,8 +31,11 @@
     </div>
 
 </template>
-
-
+<script lang="ts">
+    export default {
+      name:"kCarouselcard",
+    }
+</script>
 <script lang="ts" setup>
 import { ref, toRef, watch,onMounted } from "vue"
 const props = defineProps({
--- src/views/Carousel/doc/demo2.vue
+++ src/views/Carousel/doc/demo2.vue
@@ -10,7 +10,6 @@
     </div>
 </template>
 <script lang="ts" setup>
-import kCarousel from "../KCarousel.vue";
 let imgData = [
     {
         url: "https://img-blog.csdnimg.cn/bc3292108e9c4f3ab289d32ff8fba599.jpg",
