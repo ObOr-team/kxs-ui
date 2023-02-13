fffa0c814daa333cd4c7751ff1c32176e4a77708
Sanfer <3232793142@qq.com>
2023-02-13 20:50:45 +0800

    code preview

---
+++ src/views/Icon/doc/demo.vue
@@ -0,0 +1,8 @@
+<template>
+    <i class="k-icon-yanjing" style="font-size:25px;margin-right:20px"></i>
+    <i class="k-icon-guanbi" style="font-size:25px;margin-right:20px"></i>
+    <i class="k-icon-shezhi-xianxing1" style="font-size:25px;margin-right:20px"></i>
+    <i class="k-icon-translate_line" style="font-size:25px;margin-right:20px"></i>
+    <i class="k-icon-jiazai" style="font-size:25px;margin-right:20px"></i>
+    <i class="k-icon-github_line" style="font-size:25px;margin-right:20px"></i>
+</template>
---
+++ src/views/Icon/doc/doc.md
@@ -0,0 +1,20 @@
+<script setup>
+import demo from './demo.vue'
+import index from '../index.vue'
+</script>
+
+# Icon 图标
+
+#### 提供了 170 多个常用的 icon 图标
+
+## 使用方法
+
+<demo/>
+
+#### 给元素设置类名 k-icon-iconName 即可。
+
+<div class="borderBox">
+    <k-preview compname="Icon" demoname="demo" open/>
+</div>
+
+<index/>
