4cc50313e7d31df1fa1ec76157045870e37406ed
Huangpeijin <kim475@163.com>
2023-02-15 21:46:03 +0800

    Feat(vp-button):The button component preview is complete

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,10 +4,11 @@ import kButton from '../../../components/button.vue'
 import kInput from '../../../components/input.vue'
 import kRadio from '../../../components/radio.vue'
 import kSelect from '../../../components/select.vue'
+import kSpace from '../../../components/space.vue'
 import "./custom.scss"
 import "../../../src/style.scss"
 
-const components = [kButton,kInput,kRadio,kSelect]
+const components = [kButton,kInput,kRadio,kSelect,kSpace]
 export default {
   ...VuetomTheme,
   enhanceApp({ app }) {
