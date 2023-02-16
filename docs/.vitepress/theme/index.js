f2a25228f0c578c996216584ca943c881e13ebff
Huangpeijin <kim475@163.com>
2023-02-16 15:22:47 +0800

    Feat:Component registration completed

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -5,10 +5,14 @@ import kInput from '../../../components/input.vue'
 import kRadio from '../../../components/radio.vue'
 import kSelect from '../../../components/select.vue'
 import kSpace from '../../../components/space.vue'
+import kCarousel from '../../../components/carousel.vue'
+import kOptfile from '../../../components/optfile.vue'
+import kCarouselcard from '../../../components/carouselcard.vue'
+import kTextarea from '../../../components/textarea.vue'
 import "./custom.scss"
 import "../../../src/style.scss"
 
-const components = [kButton,kInput,kRadio,kSelect,kSpace]
+const components = [kButton,kInput,kRadio,kSelect,kSpace,kCarousel,kCarouselcard,kOptfile,kSelect,kTextarea]
 export default {
   ...VuetomTheme,
   enhanceApp({ app }) {
